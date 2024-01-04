293eac5f0c327ff29bf5542b4ee17883b8fe1279
DavyGevaert <gevaert.davy@gmail.com>
2024-01-04 12:22:32 +0100

    Add project files.

---
+++ Pennywise.BlazorWA/Model/Balloon.cs
@@ -0,0 +1,10 @@
+﻿namespace Pennywise.Model
+{
+    public class Balloon
+    {
+        public int PositionTop { get; set; }
+        public int PositionLeft { get; set; }
+        public bool IsPopped { get; set; }
+        public string ImageFileName { get; set; }
+    }
+}
